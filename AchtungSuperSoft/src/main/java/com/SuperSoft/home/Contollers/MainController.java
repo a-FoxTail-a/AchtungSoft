5aa6a7a81591b891a9ca66dda7939cfa56308a0c
FoxTaiL <FoxTail@test.ru>
2020-04-03 12:47:52 +0300

    web_v2.0
    Signed-off-by: FoxTaiL <FoxTail@test.ru>

--- AchtungSuperSoft/src/main/java/com/SuperSoft/home/Contollers/MainController.java
+++ AchtungSuperSoft/src/main/java/com/SuperSoft/home/Contollers/MainController.java
@@ -1,10 +1,10 @@
 package com.SuperSoft.home.Contollers;
 
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -29,11 +29,33 @@ public class MainController {
         return "PageProtocol";
     }
 
+//    @GetMapping("/protocolJMS")
+//    public String protocolJMS(Model model) {
+//        model.addAttribute("Protocol", "JMS");
+//        model.addAttribute("ResponseDB", "Тут будет ответ от сервера");
+//        return "PageProtocol";
+//    }
+
+//
+//    (@RequestParam Integer idProvider,
+//    @RequestParam String department,
+//    @RequestParam String carNumber,
+//    @RequestParam ("arrivalDate") @DateTimeFormat(iso = ISO.DATE) LocalDate startDate,
+//    @RequestParam("arrivalDate") @DateTimeFormat(iso = ISO.DATE) LocalDate endDate,
+//    @ModelAttribute("idAttribute") Supply supply, Model model)
+
+
     @GetMapping("/protocolJMS")
-    public String protocolJMS(Model model) {
-        model.addAttribute("Protocol", "SOAP");
-        model.addAttribute("ResponseDB", "Тут будет ответ от сервера");
+    public String protocolJMS(@RequestParam(name="name", required=false, defaultValue="Тут будет ответ от БД") String name,
+                              @RequestParam(name="email", required=false, defaultValue="") String email,Model model) {
+        model.addAttribute("Protocol", "JMS");
+        model.addAttribute("ResponseDB", name + email);
+
         return "PageProtocol";
     }
+
+
+
+    //http://localhost:8080/protocolJMSCreate?name=123&email=123&other=123
 
 }
